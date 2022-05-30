d00c7f1bf0ac7cf7417a094c8ea11f940a3c1d16
Coletinn <gustavocoleto@hotmail.com>
2022-05-30 19:54:11 -0300

    Removido codigo anterior e adicionado versao FINAL do projeto

---
+++ tartaruga_versaofinal.c
@@ -0,0 +1,37 @@
+// Gustavo Coleto de Freitas 32076541
+// Joao Vitor Teles Centrone 32033125
+// Leonardo Donda 32087535
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+double soma_global;
+double valor = 1000000000;
+int qtd_threads = 4;
+
+double calculoSerieTaylor()
+{
+	int th = omp_get_thread_num();
+  	
+    double somaLocal;
+  	double valorMinimo = (valor / qtd_threads) * th;
+	double valorMaximo = (valor / qtd_threads) + valorMinimo;
+  	for (double i = valorMinimo + 1; i <= valorMaximo; i++){
+		somaLocal += 1 / i;
+	}
+
+	#pragma omp critical
+	soma_global += somaLocal;
+}
+
+int main()
+{	
+	double t;
+
+	#pragma omp parallel num_threads(qtd_threads)
+	{
+		calculoSerieTaylor();
+	}	 
+	printf("Resultado: %f\n",soma_global);
+}
