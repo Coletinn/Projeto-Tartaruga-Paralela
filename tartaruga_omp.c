ddc086104491f1acb4a5a0683817d994babffa3d
Coletinn <gustavocoleto@hotmail.com>
2022-05-30 19:47:38 -0300

    Adicionado versao FINAL do projeto tartaruga

--- tartaruga_omp.c
+++ tartaruga_omp.c
@@ -3,42 +3,35 @@
 // Leonardo Donda 32087535
 
 #include <stdio.h>
-#include <omp.h>
 #include <stdlib.h>
-  
-void calculoSerieTaylor(int num, double* res_global) 
-{
-    // double para suportar valores mais altos
-    double soma = 0.0;
-    
-    // pragma omp for para delegar as tarefas para as threads, paralelizando o programa
-    # pragma omp for
-	for(int i = 1; i <= num; i++) 
-    {
-        soma += (double) 1 / i;
-    }
+#include <omp.h>
 
-    // fazendo o join nas threads
-    # pragma omp critical
-	*res_global += soma;
-    
-} 
- 
-int main(int argc, char* argv[]) 
-{
-	double soma_global = 0.0; 						
-	unsigned long long int n; 				
-	int qtd_threads;		
- 
-	qtd_threads = strtol(argv[1], NULL, 10);
-	printf("Digite um valor: ");
-	scanf("%lld", &n);
+double soma_global;
+double valor = 1000000000;
+int qtd_threads = 4;
 
-    # pragma omp parallel num_threads(qtd_threads)
-    calculoSerieTaylor(n, &soma_global);
- 
-	printf("\nA soma da serie de Taylor eh: %f\n", soma_global);
- 
-	return 0;
-} 
- 
+double calculoSerieTaylor()
+{
+	int th = omp_get_thread_num();
+  	
+    double somaLocal;
+  	double valorMinimo = (valor / qtd_threads) * th;
+	double valorMaximo = (valor / qtd_threads) + valorMinimo;
+  	for (double i = valorMinimo + 1; i <= valorMaximo; i++){
+		somaLocal += 1 / i;
+	}
+
+	#pragma omp critical
+	soma_global += somaLocal;
+}
+
+int main()
+{	
+	double t;
+
+	#pragma omp parallel num_threads(qtd_threads)
+	{
+		calculoSerieTaylor();
+	}	 
+	printf("A soma eh: %f\n",soma_global);
+}
